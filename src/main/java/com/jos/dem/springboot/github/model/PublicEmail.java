fb0630c3de7848ba3747601a8d837f7c2a024587
josdem <joseluis.delacruz@gmail.com>
2018-12-28 10:54:21 -0500

    Adding public email and ssh key models

---
+++ src/main/java/com/jos/dem/springboot/github/model/PublicEmail.java
@@ -0,0 +1,29 @@
+package com.jos.dem.retrofit.workshop.model;
+
+import lombok.Data;
+
+@Data
+public class PublicEmail {
+  private String email;
+  private boolean verified;
+  private boolean primary;
+  private String visibility;
+
+  public String getEmail(){
+    return email;
+  }
+
+  public boolean isVerified(){
+    return verified;
+  }
+
+  public boolean isPrimary(){
+    return primary;
+  }
+
+  public String getVisibility(){
+    return visibility;
+  }
+
+}
+
---
+++ src/main/java/com/jos/dem/springboot/github/model/SSHKey.java
@@ -0,0 +1,7 @@
+package com.jos.dem.retrofit.workshop.model;
+
+public class SSHKey {
+  Long id;
+  String key;
+}
+
